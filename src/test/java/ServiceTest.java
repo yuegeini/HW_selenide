205e699de8425a325957d31f1dccc31918697f1c
Name <barmatine99@gmail.com>
2023-10-27 22:39:17 +0300

    Test_ci#3

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -9,7 +8,7 @@ import static com.codeborne.selenide.Selectors.byText;
 
 public class ServiceTest {
     @Test
-    void shouldTest() throws InterruptedException {
+    void shouldTest() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
@@ -18,10 +17,6 @@ public class ServiceTest {
         $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-//        $(".button__text:contains('Забронировать')").click();
-//        Thread.sleep(10000);
-//        Wait(15000).until(not(hidden), $("[data-test-id=notification]"));
         $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
-
 }
