e2f44812dcab8ac671d0d220d3a50f68107617b2
Name <barmatine99@gmail.com>
2023-10-27 22:34:08 +0300

    Test_ci#2

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -13,12 +15,13 @@ public class ServiceTest {
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").sendKeys(Keys.ESCAPE);
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
 //        $(".button__text:contains('Забронировать')").click();
-        Thread.sleep(10000);
-        $("[data-test-id=notification]").shouldNotBe(hidden);
+//        Thread.sleep(10000);
+//        Wait(15000).until(not(hidden), $("[data-test-id=notification]"));
+        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
 
 }
