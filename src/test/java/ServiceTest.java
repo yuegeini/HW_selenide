4f8a82d0382a5a74653291e9f320bc73979c7110
Name <barmatine99@gmail.com>
2023-11-02 22:26:02 +0300

    del hard date

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,32 +1,45 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
 
 public class ServiceTest {
+
+    int daysToAddForFirstMeeting = 3;
+    String MeetingDate = generateDate(daysToAddForFirstMeeting);
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
     public  void be(){
+
         open("http://localhost:9999");
     }
     @Test
     void shouldTest() {
+
+
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-                //.shouldBe(Condition.attribute("cursor",
-               // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(MeetingDate);
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + MeetingDate))
+                .shouldBe(visible);
+
     }
 
     @Test
@@ -34,34 +47,22 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казан");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-        //.shouldBe(Condition.attribute("cursor",
-        // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(MeetingDate);
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=city] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
-//        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
     @Test
     void shouldFailName() {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий1");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-        //.shouldBe(Condition.attribute("cursor",
-        // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(MeetingDate);
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
 
     @Test
@@ -69,17 +70,11 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+792700000100");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-        //.shouldBe(Condition.attribute("cursor",
-        // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(MeetingDate);
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-//        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
 
     @Test
@@ -87,34 +82,24 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-        //.shouldBe(Condition.attribute("cursor",
-        // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("31.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
-//        $("[data-test-id=agreement]").click();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(MeetingDate);
         $(byText("Забронировать")).click();
         $("[data-test-id=agreement].invalid_input").shouldBe();
-//        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
 
     @Test
     void shouldFailDate() {
+
+        int newDaysToAddForFirstMeeting = 1;
+        String newMeetingDate = generateDate(newDaysToAddForFirstMeeting);
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").click();
-//        $(".popup .calendar__day.popup_visible").shouldBe(visible).click();
-        //.shouldBe(Condition.attribute("cursor",
-        // "pointer"))
-
-        $("[data-test-id=date] input").doubleClick().sendKeys("20.10.2023");//.setValue();
-        $("[data-test-id=date] input").pressEscape();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(newMeetingDate);
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=date] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
-//        $("[data-test-id=notification]").shouldBe(not(hidden), Duration.ofSeconds(15));
     }
 }
