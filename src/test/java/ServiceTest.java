43de3638357b1602e6323848ace40d280192ce4f
Name <barmatine99@gmail.com>
2023-11-02 22:35:57 +0300

    repair tests#2

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -30,8 +30,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(MeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(MeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
@@ -47,8 +49,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казан");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(MeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(MeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=city] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
@@ -58,8 +62,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий1");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(MeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(MeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -70,8 +76,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+792700000100");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(MeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(MeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
@@ -82,8 +90,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(MeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(MeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $(byText("Забронировать")).click();
         $("[data-test-id=agreement].invalid_input").shouldBe();
     }
@@ -96,8 +106,10 @@ public class ServiceTest {
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=name] input").setValue("Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(newMeetingDate);
+        $("[data-test-id=date] input").click();
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(newMeetingDate);//.setValue();
+        $("[data-test-id=date] input").pressEscape();
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=date] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
